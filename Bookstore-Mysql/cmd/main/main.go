02ab40e834646f1bcc9147602f333760229c5387
devPryze <msaaddev@gmail.com>
2022-08-01 18:05:00 -0700

    added mysql util

---
+++ Bookstore-Mysql/cmd/main/main.go
@@ -0,0 +1,5 @@
+package main
+
+import (
+	_ "gorm.io/driver/mysql"
+)
---
+++ Bookstore-Mysql/pkg/models/book.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"github.com/wickedknock/Bookstore-Mysql/pkg/config"
+	"github.com/wickedknock/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
