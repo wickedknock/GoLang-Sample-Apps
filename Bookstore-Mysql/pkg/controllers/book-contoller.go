f0d9e892a092b7cd8382dfcc37455b8c6de79d97
devPryze <msaaddev@gmail.com>
2022-08-04 15:11:36 -0700

    finished sample REST API with CRUD using gorm and mysql

--- Bookstore-Mysql/pkg/controllers/book-contoller.go
+++ Bookstore-Mysql/pkg/controllers/book-contoller.go
@@ -60,5 +60,27 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
-
+	var updateBook = &model.Book{}
+	util.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error parsing book")
+	}
+	bookDetails, db := model.GetBookById(ID)
+	if updateBook.Name != "" {
+		bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		bookDetails.Publication = updateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- Bookstore-Mysql/pkg/routes/book-route.go
+++ Bookstore-Mysql/pkg/routes/book-route.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controller.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controller.GetBook).Methods("GET")
-	router.HandleFunc("/book/{id}", controller.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{id}", controller.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{id}", controller.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
 }
